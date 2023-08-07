75848393a5beca4390877699789511979808599d
Dhruv Lamba <lambadhruv09@gmail.com>
2023-08-07 20:33:18 +0530

    Added categories

---
+++ src/components/News.js
@@ -0,0 +1,126 @@
+import React, { Component } from "react";
+import Newsitem from "./Newsitem";
+import Spinner from "./Spinner";
+// import { useState } from "react";
+import PropTypes from "prop-types";
+export default class News extends Component {
+  static defaultProps = {
+    country: "in",
+    pageSize: 8,
+    category: "general",
+    img: "https://static.toiimg.com/photo/72975551.cms",
+  };
+  static propTypes = {
+    country: PropTypes.string,
+    pageSize: PropTypes.number,
+    category: PropTypes.string,
+  };
+  constructor() {
+    super();
+    this.state = {
+      articles: [],
+      page: 1,
+      loading: false,
+    };
+  }
+
+  async componentDidMount() {
+    let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=e29d1defa4114e02b9783b1586b73854&page=1&pageSize=${this.props.pageSize}`;
+    this.setState({ loading: true });
+    let data = await fetch(url);
+    let parsedData = await data.json();
+    this.setState({
+      articles: parsedData.articles,
+      totalArticles: parsedData.totalResults,
+      loading: false,
+    });
+  }
+  handlenext = async () => {
+    if (
+      !(
+        this.state.page + 1 >
+        Math.ceil(this.state.totalResults / this.props.pageSize)
+      )
+    ) {
+      let url = `https://newsapi.org/v2/top-headlines?country=${
+        this.props.country
+      }&category=${
+        this.props.category
+      }&apiKey=e29d1defa4114e02b9783b1586b73854&page=${
+        this.state.page + 1
+      }&pageSize=${this.props.pageSize}`;
+      this.setState({ loading: true });
+      let data = await fetch(url);
+      let parsedData = await data.json();
+      this.setState({
+        articles: parsedData.articles,
+        page: this.state.page + 1,
+        loading: false,
+      });
+    }
+  };
+
+  handleprevious = async () => {
+    console.log("Dhruv");
+    let url = `https://newsapi.org/v2/top-headlines?country=${
+      this.props.country
+    }&category=${
+      this.props.category
+    }&apiKey=e29d1defa4114e02b9783b1586b73854&page=${
+      this.state.page - 1
+    }&pageSize=${this.props.pageSize}`;
+    this.setState({ loading: true });
+    let data = await fetch(url);
+    let parsedData = await data.json();
+    this.setState({
+      articles: parsedData.articles,
+      page: this.state.page - 1,
+      loading: false,
+    });
+  };
+
+  render() {
+    return (
+      <div className="container my-3">
+        <h2 className="text-center my-4">News Monkey Top Headlines</h2>
+        {this.state.loading && <Spinner />}
+        <div className="row">
+          {!this.state.loading &&
+            this.state.articles.map((element) => {
+              return (
+                <div className="col-md-4" key={element.url}>
+                  <Newsitem
+                    title={element.title}
+                    description={element.description}
+                    img={element.urlToImage}
+                    newsurl={element.url}
+                  />
+                </div>
+              );
+            })}
+        </div>
+        <div className="container d-flex justify-content-between my-3">
+          <button
+            type="button"
+            disabled={this.state.page <= 1}
+            onClick={this.handleprevious}
+            className="btn btn-dark"
+          >
+            &larr; Previous
+          </button>
+          <button
+            type="button"
+            disabled={
+              this.state.page + 1 >
+              Math.ceil(this.state.totalArticles / this.props.pageSize)
+            }
+            onClick={this.handlenext}
+            className="btn btn-dark"
+          >
+            Next &rarr;
+          </button>
+        </div>
+      </div>
+    );
+  }
+}
